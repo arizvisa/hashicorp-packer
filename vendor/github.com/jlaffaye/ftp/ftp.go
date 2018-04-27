4c52f2c657d08208019b2ba778223e9b52ac0b0f
Ali Rizvi-Santiago <arizvisa@gmail.com>
2018-04-27 01:26:44 -0500

    Updated vendor package github.com/jlaffaye/ftp to commit 2403248fa8cc9f7909862627aa7337f13f8e0bf1

--- vendor/github.com/jlaffaye/ftp/ftp.go
+++ vendor/github.com/jlaffaye/ftp/ftp.go
@@ -30,6 +30,9 @@ type ServerConn struct {
 	// Do not use EPSV mode
 	DisableEPSV bool
 
+	// Timezone that the server is in
+	Location *time.Location
+
 	conn          *textproto.Conn
 	host          string
 	timeout       time.Duration
@@ -83,6 +86,7 @@ func DialTimeout(addr string, timeout time.Duration) (*ServerConn, error) {
 		host:     remoteAddr.IP.String(),
 		timeout:  timeout,
 		features: make(map[string]string),
+		Location: time.UTC,
 	}
 
 	_, _, err = c.conn.ReadResponse(StatusReady)
@@ -216,7 +220,7 @@ func (c *ServerConn) epsv() (port int, err error) {
 }
 
 // pasv issues a "PASV" command to get a port number for a data connection.
-func (c *ServerConn) pasv() (port int, err error) {
+func (c *ServerConn) pasv() (host string, port int, err error) {
 	_, line, err := c.cmd(StatusPassiveMode, "PASV")
 	if err != nil {
 		return
@@ -226,14 +230,16 @@ func (c *ServerConn) pasv() (port int, err error) {
 	start := strings.Index(line, "(")
 	end := strings.LastIndex(line, ")")
 	if start == -1 || end == -1 {
-		return 0, errors.New("Invalid PASV response format")
+		err = errors.New("Invalid PASV response format")
+		return
 	}
 
 	// We have to split the response string
 	pasvData := strings.Split(line[start+1:end], ",")
 
 	if len(pasvData) < 6 {
-		return 0, errors.New("Invalid PASV response format")
+		err = errors.New("Invalid PASV response format")
+		return
 	}
 
 	// Let's compute the port number
@@ -251,15 +257,18 @@ func (c *ServerConn) pasv() (port int, err error) {
 
 	// Recompose port
 	port = portPart1*256 + portPart2
+
+	// Make the IP address to connect to
+	host = strings.Join(pasvData[0:4], ".")
 	return
 }
 
-// getDataConnPort returns a port for a new data connection
+// getDataConnPort returns a host, port for a new data connection
 // it uses the best available method to do so
-func (c *ServerConn) getDataConnPort() (int, error) {
+func (c *ServerConn) getDataConnPort() (string, int, error) {
 	if !c.DisableEPSV {
 		if port, err := c.epsv(); err == nil {
-			return port, nil
+			return c.host, port, nil
 		}
 
 		// if there is an error, disable EPSV for the next attempts
@@ -271,12 +280,12 @@ func (c *ServerConn) getDataConnPort() (int, error) {
 
 // openDataConn creates a new FTP data connection.
 func (c *ServerConn) openDataConn() (net.Conn, error) {
-	port, err := c.getDataConnPort()
+	host, port, err := c.getDataConnPort()
 	if err != nil {
 		return nil, err
 	}
 
-	return net.DialTimeout("tcp", net.JoinHostPort(c.host, strconv.Itoa(port)), c.timeout)
+	return net.DialTimeout("tcp", net.JoinHostPort(host, strconv.Itoa(port)), c.timeout)
 }
 
 // cmd is a helper function to execute a command and check for the expected FTP
@@ -369,7 +378,7 @@ func (c *ServerConn) List(path string) (entries []*Entry, err error) {
 	scanner := bufio.NewScanner(r)
 	now := time.Now()
 	for scanner.Scan() {
-		entry, err := parser(scanner.Text(), now)
+		entry, err := parser(scanner.Text(), now, c.Location)
 		if err == nil {
 			entries = append(entries, entry)
 		}
